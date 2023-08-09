8c32e015bfcf45d482ef7a1a0c349f180341d4a1
MamadTvl <mtavallai80@gmail.com>
2023-08-09 17:42:47 +0330

    add class-validator

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.useGlobalPipes(new ValidationPipe({ transform: true }));
     await import('@adminjs/mikroorm').then(async ({ Database, Resource }) => {
         const { AdminJS } = await import('adminjs');
         return AdminJS.registerAdapter({ Database, Resource });
