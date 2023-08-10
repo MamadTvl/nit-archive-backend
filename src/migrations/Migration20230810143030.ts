9f1d1609608b3ec718d18b8c95b7a71677af02d3
MamadTvl <mtavallai80@gmail.com>
2023-08-10 20:34:01 +0330

    format files

--- src/migrations/Migration20230810143030.ts
+++ src/migrations/Migration20230810143030.ts
@@ -1,15 +1,15 @@
 import { Migration } from '@mikro-orm/migrations';
 
 export class Migration20230810143030 extends Migration {
+    async up(): Promise<void> {
+        this.addSql('alter table `videos` drop index `videos_slug_unique`;');
+        this.addSql('alter table `videos` drop `slug`;');
+    }
 
-  async up(): Promise<void> {
-    this.addSql('alter table `videos` drop index `videos_slug_unique`;');
-    this.addSql('alter table `videos` drop `slug`;');
-  }
-
-  async down(): Promise<void> {
-    this.addSql('alter table `videos` add `slug` varchar(255) null;');
-    this.addSql('alter table `videos` add unique `videos_slug_unique`(`slug`);');
-  }
-
+    async down(): Promise<void> {
+        this.addSql('alter table `videos` add `slug` varchar(255) null;');
+        this.addSql(
+            'alter table `videos` add unique `videos_slug_unique`(`slug`);',
+        );
+    }
 }
