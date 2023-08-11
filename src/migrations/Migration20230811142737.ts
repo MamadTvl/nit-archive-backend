d4f3ae8944b5a1ac21275eab87d9412818cfc6f6
MamadTvl <mtavallai80@gmail.com>
2023-08-11 21:09:19 +0330

    reformat

--- src/migrations/Migration20230811142737.ts
+++ src/migrations/Migration20230811142737.ts
@@ -1,13 +1,11 @@
 import { Migration } from '@mikro-orm/migrations';
 
 export class Migration20230811142737 extends Migration {
+    async up(): Promise<void> {
+        this.addSql('alter table `users` add `media` json not null;');
+    }
 
-  async up(): Promise<void> {
-    this.addSql('alter table `users` add `media` json not null;');
-  }
-
-  async down(): Promise<void> {
-    this.addSql('alter table `users` drop `media`;');
-  }
-
+    async down(): Promise<void> {
+        this.addSql('alter table `users` drop `media`;');
+    }
 }
--- src/migrations/Migration20230811142845.ts
+++ src/migrations/Migration20230811142845.ts
@@ -1,21 +1,19 @@
 import { Migration } from '@mikro-orm/migrations';
 
 export class Migration20230811142845 extends Migration {
+    async up(): Promise<void> {
+        this.addSql('alter table `categories` modify `media` json null;');
 
-  async up(): Promise<void> {
-    this.addSql('alter table `categories` modify `media` json null;');
+        this.addSql('alter table `courses` modify `media` json null;');
 
-    this.addSql('alter table `courses` modify `media` json null;');
+        this.addSql('alter table `users` modify `media` json null;');
+    }
 
-    this.addSql('alter table `users` modify `media` json null;');
-  }
+    async down(): Promise<void> {
+        this.addSql('alter table `categories` modify `media` json not null;');
 
-  async down(): Promise<void> {
-    this.addSql('alter table `categories` modify `media` json not null;');
+        this.addSql('alter table `courses` modify `media` json not null;');
 
-    this.addSql('alter table `courses` modify `media` json not null;');
-
-    this.addSql('alter table `users` modify `media` json not null;');
-  }
-
+        this.addSql('alter table `users` modify `media` json not null;');
+    }
 }
